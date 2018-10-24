2e64867ff5dc8541850dd5a620a85b0673028849
Ahuja, Navpreet <a592605@fil.com>
2018-10-24 11:40:17 +0530

    minor fix

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,16 +5,21 @@ import Backdrop from '../Backdrop/Backdrop';
 
 // class based, so that modal updates only when visible
 class Modal extends Component {
+
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
     render () {
         return (
             <div>
-                <Backdrop show={props.show} clicked={props.modalClosed} />
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
                 <div className={classes.Modal}
                     style={{
-                        transform: props.show ? 'translateY(0)' : 'translateY(-100%)',
-                        opacity: props.show ? '1' : '0'
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100%)',
+                        opacity: this.props.show ? '1' : '0'
                     }}>
-                    {props.children}
+                    {this.props.children}
                 </div>
             </div>
         );
