b32aef2021e70593e94bf05d13aec56ad9ab55b7
Ahuja, Navpreet <a592605@fil.com>
2018-11-06 12:23:10 +0530

    axios with firebase added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -76,7 +77,13 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        console.log('continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+
+        };
+        // orders node is created, .json added for firebase
+        axios.post('/orders.json', order);
     }
 
     render () {
