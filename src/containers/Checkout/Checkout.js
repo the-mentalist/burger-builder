e143838b2b8ae9ed9d56e30709ec15bc86d09d15
Ahuja, Navpreet <a592605@fil.com>
2018-11-23 16:42:16 +0530

    checkout added

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+    
+    render () {
+        return (
+            <CheckoutSummary />
+        );
+    }
+}
+
+export default Checkout;
