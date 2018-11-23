b9a12b7b2076f8722e0d398a07a42394c031950d
Ahuja, Navpreet <a592605@fil.com>
2018-11-23 16:46:27 +0530

    checkout added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, }
+import { Switch, Route} from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
