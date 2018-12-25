2d56fe61513aae06c31dfaa2d8db87809c4e2437
navpreet.ahuja@hotmail.com <navpreet.ahuja@hotmail.com>
2018-12-26 01:27:28 +0530

    buttons changed

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,18 +3,18 @@ import React from 'react';
 import classes from './BuildControl.css';
 
 const buildControl = (props) => {
-    let quantity = props.quantity ? <div className={classes.TextLabel}><label>{props.quantity}</label></div> : null;
+    let quantity = props.quantity ? props.quantity : 0;
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{props.label}</div>
             <button 
                 className={classes.Less} 
                 onClick={props.removed} 
-                disabled={props.disabled}>Less</button>
-            {quantity}
+                disabled={props.disabled}>-</button>
+            <span><input type="text" value={quantity} className={classes.TextLabel} /></span>
             <button 
                 className={classes.More} 
-                onClick={props.added}>More</button>
+                onClick={props.added}>+</button>
         </div>
     );
 };
